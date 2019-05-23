7a2541daf10576464f0d3347e6d10f26460ffceb
kieranhalliday <kieran.halliday@pm.me>
2019-05-22 18:32:01 -0700

    Kingmaker media

---
+++ Images before processing/tiles 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="tiles" tilewidth="22" tileheight="52" tilecount="4800" columns="96">
+ <image source="Kingmaker_map.jpg" width="2131" height="2618"/>
+</tileset>
